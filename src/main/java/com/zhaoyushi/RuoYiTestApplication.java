626be36012b10c737f9b546ef054a13c36662b5e
zhaoy <zhaoyushi00@gmail.com>
2025-06-03 21:21:59 +0800

    Initial commit

---
+++ src/main/java/com/zhaoyushi/RuoYiTestApplication.java
@@ -0,0 +1,13 @@
+package com.zhaoyushi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuoYiTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuoYiTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zhaoyushi/RuoYiTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhaoyushi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuoYiTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
